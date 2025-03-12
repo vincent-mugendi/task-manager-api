bfcedb699144e8db538317fdcc6f974e8f10764a
vincent-mugendi <vincentmwathi5@gmail.com>
2025-03-13 01:36:39 +0300

    updating server.js to use connectDB

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config(); // Load environment variables
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(express.json()); // Middleware to parse JSON data
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
